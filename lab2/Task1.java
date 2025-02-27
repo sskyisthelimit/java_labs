25078ac0d2636e08f7713547251a61d7c22880e2
Yalanskyi Roman <keito.eternal@gmail.com>
2025-02-27 17:27:10 +0200

    modif: make task1 execution more understandable

--- lab2/Task1.java
+++ lab2/Task1.java
@@ -1,13 +1,25 @@
 package lab2;
 import lab2.Circle;
-
+import java.util.Scanner;
 public class Task1 {
     public static void main(String[] args) {
-        Circle c = new Circle();
+        Scanner scanner = new Scanner(System.in);
 
+        Circle c = new Circle();
+        System.out.println("Trying to calc. area without setting class prop.: ");
         System.out.println("Area: " + c.calcArea());
-        c.setRadius(5);
-        c.setCenterCoords(2, 3);
+        System.out.println("Set circle radius:");
+
+        float radius = scanner.nextFloat();
+        c.setRadius(radius);
+
+        System.out.println("Set x coord for circle:");
+        float x_coord = scanner.nextFloat();
+
+        System.out.println("Set y coord for circle:");
+        float y_coord = scanner.nextFloat();
+
+        c.setCenterCoords(x_coord, y_coord);
 
         System.out.println("Area: " + c.calcArea());
     }
